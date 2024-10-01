b1d25120b47c43be46afac60a926712d5668a88e
Sarhad Bahrami <Sarhad94127@hotmail.com>
2024-10-01 13:18:47 +0200

    first commit

---
+++ src/main/java/org/sarhad/marketking/MarketKingApplication.java
@@ -0,0 +1,13 @@
+package org.sarhad.marketking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sarhad/marketking/MarketKingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sarhad.marketking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
